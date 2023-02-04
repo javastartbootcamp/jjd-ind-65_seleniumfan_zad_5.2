30eaccfa08fa285b543151c6318573bb135f7c49
Tomasz Iwanicki <tiko35@o2.pl>
2023-02-05 00:57:33 +0100

    - Adding room and method to compute temperature

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,22 @@
 package pl.javastart.task;
 
 public class Main {
-
     public static void main(String[] args) {
-
+        Room room1 = new Room(10, 23.2, true);
+        showRoomInfo(room1);
+        room1.decreaseTemperature();
+        showRoomInfo(room1);
+        room1.decreaseTemperature();
+        showRoomInfo(room1);
+        room1.decreaseTemperature();
+        showRoomInfo(room1);
+        room1.decreaseTemperature();
+        showRoomInfo(room1);
+        room1.decreaseTemperature();
     }
+
+    private static void showRoomInfo(Room room) {
+        System.out.println("Powierzchnia: " + room.getArea() + ", temperature: " + room.getTemperature() + ", airConditioner: " + room.isAirConditioner());
+    }
+
 }
